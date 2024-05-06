ff0ed18994a6e01e4e83d216c129c9222daf7a92
gitya780 <giteshewatkar@gmail.com>
2024-05-05 22:44:57 -0400

    assignment 4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <errno.h>
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
@@ -10,24 +10,37 @@
 
 void* threadfunc(void* thread_param)
 {
-
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    usleep(thread_func_args->wait_to_obtain_ms* 1000);
+    pthread_mutex_lock(thread_func_args->mutex);
+    usleep(thread_func_args->wait_to_release_ms* 1000);
+    pthread_mutex_unlock(thread_func_args->mutex);
+    thread_func_args->thread_complete_success = true;
     return thread_param;
+   
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
-    return false;
+
+int ret;
+struct thread_data* para =(struct thread_data*)malloc(sizeof(struct thread_data));
+if (!para){
+		ERROR_LOG("Failed to allocate thread_data");
+		return false;
+	}
+    para->mutex=mutex;
+    para->wait_to_obtain_ms=wait_to_obtain_ms;
+    para->wait_to_release_ms=wait_to_release_ms;
+    para->thread_complete_success = false;
+    ret= pthread_create( thread, NULL,threadfunc,para);
+    if(ret!=0){
+      perror("pthread_create");
+      free(para);
+      return false;
+     }
+
+ return true;
 }
 
